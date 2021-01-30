05ab743214b180b2edff6e1379bf170ed2097002
melindali255 <contact.melindali@gmail.com>
2021-01-29 23:19:58 -0800

    Comments are removed if they contain a given word

--- get_comments.js
+++ get_comments.js
@@ -1,12 +1,24 @@
 $(document).on('mouseover mouseout', '#comments', function() {
-    let comments = document.querySelectorAll('yt-formatted-string.ytd-comment-renderer');
 
-    console.log("length of comments: " + comments.length);
+    /* Highlights individual comment texts */
+    // let comments = document.querySelectorAll('yt-formatted-string.ytd-comment-renderer');
 
-    for (let i = 0; i < comments.length; i++) {
-        if (!comments[i].classList.contains("published-time-text")) {
-            comments[i].style["background-color"] = "red";
+    // for (let i = 0; i < comments.length; i++) {
+    //     if (!comments[i].classList.contains("published-time-text")) {
+    //         comments[i].style["background-color"] = "red";
+    //     }
+    // }
+
+    /* Remove entire comment */
+    let comment_threads = document.getElementsByTagName("ytd-comment-thread-renderer");
+
+    // console.log("length of comments threads: " + comment_threads.length);
+
+    for (let i = 0; i < comment_threads.length; i++) {
+        let comment = comment_threads[i].querySelector("#content-text");
+        if (comment.textContent.includes("Me")) { //Remove comment if text includes the word
+            // comment.style["background-color"] = "red";
+            comment_threads[i].parentNode.removeChild(comment_threads[i]);
         }
     }
-
 });
